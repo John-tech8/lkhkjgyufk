4ced1cefa43cd1bd269e8a3724cb55903617f582
Roshan G <roshan.devcodes@gmail.com>
2026-04-30 21:08:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1> My Favourite Food </h1>
+    <ul>
+      <li>Biriyani</li>
+      <li>Noodles</li>
+      <li>Fried Rice</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
